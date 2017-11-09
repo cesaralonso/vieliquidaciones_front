d8c550bc40f506a7cd65b71242287f93140a7713
Armando Lopez <galle2010@live.com.mx>
2017-11-09 15:11:55 -0600

    vehiculoreparandos

---
+++ src/app/pages/vehiculoreparandos/components/vehiculoreparandos-table/vehiculoreparandos.interface.ts
@@ -0,0 +1,15 @@
+export interface VehiculoreparandosInterface {
+
+    idvehiculoreparando?: number;
+    taller_idtaller: number;
+    enviotaller_idenviotaller: number;
+    fechaIngresa: string;
+    mecanico_idmecanico: number;
+    fechaSalida: string;
+    fechaEstimada: string;
+    inventario:string;
+    motivo:string;
+    status: string;
+    orden:string;
+    permisotaxiasignado_idpermisotaxiasignado:number;
+}
