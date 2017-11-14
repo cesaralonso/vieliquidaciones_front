1667f5890c9a89e2aa41881df9ce51441534167f
Brandon Villa <bornofos@gmail.com>
2017-11-13 19:24:12 -0600

    Vehiculos CRUD

--- src/app/pages/vehiculos/components/vehiculos-table/vehiculos-table.component.ts
+++ src/app/pages/vehiculos/components/vehiculos-table/vehiculos-table.component.ts
@@ -1,12 +1,9 @@
-import { UploadModalComponent } from './../../../../shared/components/upload-modal/upload-modal.component';
-import { FilesUploadModalComponent } from './../../../../shared/components/files-upload-modal/files-upload-modal.component';
 import { DialogService } from 'ng2-bootstrap-modal';
 import { ToastrService } from 'ngx-toastr';
 import { VehiculosInterface } from './vehiculos.interface';
 import { VehiculosResponseInterface } from './vehiculos-response.interface';
 import { Component, OnInit } from '@angular/core';
 import { VehiculosService } from './vehiculos.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { VehiculosAddModalComponent } from './vehiculos-add-modal/vehiculos-add-modal.component';
 import { VehiculosEditModalComponent } from './vehiculos-edit-modal/vehiculos-edit-modal.component';
 
@@ -26,13 +23,8 @@ export class VehiculosTableComponent implements OnInit {
 
     constructor(
       private vehiculosService: VehiculosService, 
-      private modalService: NgbModal, 
       private toastrService: ToastrService, 
       private dialogService: DialogService) {
-    }
-
-    toInt(num: string) {
-        return +num;
     }
 
     addVehiculosModalShow() {
@@ -46,21 +38,6 @@ export class VehiculosTableComponent implements OnInit {
           data ? this.showToast(data) : null,
           error => console.log(error),
           () => console.log('Modified complete'));
-    }
-
-    uploadModalShow(id: number, descripcion: string) {
-      const activeModal = this.modalService.open(UploadModalComponent, { size: 'lg' });
-      activeModal.componentInstance.modalHeader = 'Agregar Archivo a Vehiculo';
-      activeModal.componentInstance.id = id;
-      activeModal.componentInstance.descripcion = descripcion;
-      activeModal.componentInstance.referencia = 'Vehiculo';
-    }
-
-    filesModalShow(id: number) {
-      const activeModal = this.modalService.open(FilesUploadModalComponent, { size: 'lg' });
-      activeModal.componentInstance.modalHeader = 'Ver Archivos de Vehiculo';
-      activeModal.componentInstance.id = id;
-      activeModal.componentInstance.referencia = 'Vehiculo';
     }
 
     onDeleteConfirm(event, id): void {
